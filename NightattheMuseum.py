48567e8d4d33e72d279100bd95711bcbc42c4142
NathanE212121 <155912127+NathanE212121@users.noreply.github.com>
2025-02-12 15:19:43 +0300

    Delete NightattheMuseum.py

--- NightattheMuseum.py
+++
@@ -1,9 +0,0 @@
-name = input()
-letter = 'a'
-steps = 0
-for char in name:
-    clock = abs(ord(char)-ord(letter))
-    counter = 26 - clock
-    steps += min(clock,counter)
-    letter = char
-print(steps)
